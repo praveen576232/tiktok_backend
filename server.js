ddf204e66e1025c9f76f7c8e7eb16908c01dd563
praveen <praveend2622@gmail.com>
2020-09-03 20:29:05 +0530

    make it better6

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const schema = require('./Database/database')
 const app = new express();
 const mongoose = require('mongoose')
 app.use(express.json());
-
 const port =process.env.PORT || 9090;
 mongoose.connect('mongodb+srv://praveend:praveend123@tiktokclone.halcg.mongodb.net/tiktokdatabase?retryWrites=true&w=majority',{useNewUrlParser:true})
+
+
+app.use((req,res,next)=>{
+res.setHeader("Access-Control-Allow-Origin","*");
+res.setHeader("Access-Control-Allow-Headers","*")
+})
 
 app.get('/v1/video/get',(req,res)=>{
  
